9b324d60ab8bf4fb6b982a35593b9b1576e5599c
ProstoEgor <prosto.egor2108@gmail.com>
2020-04-19 23:23:29 +0400

    новые скрипты

---
+++ Assets/Scripts/CollisionObserver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionObserver : MonoBehaviour
+{ 
+
+    private HealthHelper _healthHelper;
+
+    void Start()
+    {
+        _healthHelper = transform.parent.GetComponent<HealthHelper>() as HealthHelper;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Robot"))
+            _healthHelper.GetRobotHit(collision);
+        else if (collision.gameObject.CompareTag("Lava"))
+            _healthHelper.Lava = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Lava"))
+            _healthHelper.Lava = false;
+    }
+}
