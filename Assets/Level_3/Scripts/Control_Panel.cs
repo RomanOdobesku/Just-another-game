bf16ecc0282312f54c9c91dd757cf6480a1dcbad
SashkaShashka <sasha151299qwe@gmail.com>
2020-05-21 01:33:55 +0400

    Перезалив

---
+++ Assets/Level_3/Scripts/Control_Panel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control_Panel : MonoBehaviour
+{
+    public GameObject MyDoor;
+    //Text OpenDoor;
+    public Text OpenDoor;
+    Door door;
+    bool inTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Transform[] transforms;
+        //transforms = GameObject.Find("Canvas").transform.GetComponentsInChildren<Transform>();
+        //foreach (Transform transform in transforms)
+        //{
+        //    if (transform.gameObject.name == "Open Door")
+        //    {
+        //        OpenDoor = transform.gameObject;
+        //        Debug.Log(OpenDoor.gameObject.name.ToString());
+        //        OpenDoor.gameObject.SetActive(false);
+        //    }
+        //}
+        door = MyDoor.GetComponent<Door>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                door.speed = 30;
+                door.go = 1;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (other.CompareTag("Robot Player"))
+        {
+            OpenDoor.gameObject.SetActive(false);
+            inTrigger = false;
+        };
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag("Robot Player"))
+        {
+            OpenDoor.gameObject.SetActive(true);
+            inTrigger = true;
+        };
+    }
+}
---
+++ Assets/Level_3/Scripts/On_NPC_Center.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_NPC_Center : MonoBehaviour
+{
+    Door door;
+    public GameObject NPC_center;
+    GameObject[] NPC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door=GameObject.Find("Door 1").GetComponent<Door>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Robot Player"))
+        {
+            door.go = -1;
+            NPC_center.gameObject.SetActive(true);
+            NPC = GameObject.FindGameObjectsWithTag("Robot");
+            Destroy(gameObject);
+        };
+    }
+}
