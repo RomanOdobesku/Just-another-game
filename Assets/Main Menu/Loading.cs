cdf7c20f21fbb8ffdace6ef7d4337366950bcb6b
SashkaShashka <sasha151299qwe@gmail.com>
2020-06-13 16:58:37 +0400

    Еще кое что добавил

---
+++ Assets/Main Menu/Loading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+    
+    public void LoadScene(int scene)
+    {
+        StartCoroutine(LoadAsync(scene));
+    }
+
+    IEnumerator LoadAsync (int scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            text.text = progress*100f + "%";
+            yield return null;
+        }
+    }
+}
